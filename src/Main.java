ef9d80c2a43b638dca8476d2fa4d904b5cf0ee60
Abdulrahman Alfahad <abdulrahman.g.alfahad@gmail.com>
2024-11-06 13:37:08 +0300

    Created new exceptions and fixed the catch block, fixed display

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,50 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static class InvalidCredentialsException extends Exception {
+        public InvalidCredentialsException(String message) {
+            super(message);
+        }
+    }
+
+    static class MaximumAttemptsExceededException extends Exception {
+        public MaximumAttemptsExceededException(String message) {
+            super(message);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String validUsername = "Coded";
         String validPassword = "Coded123";
-        Exception invalid = new Exception("Invalid credentials");
-        Exception maximum = new Exception("Maximum attempts exceeded");
         for (int i = 1; i <= 5; i++) {
             try {
+                System.out.println("\n" + "=================" );
                 System.out.println("Attempt: " + i);
                 System.out.println("Enter your your username:");
                 String username = scanner.next();
                 System.out.println("Enter your password:");
                 String password = scanner.next();
-                if (i == 5){
-                    throw maximum;
-                }
                 if (!username.equals(validUsername) || !password.equals(validPassword)) {
-                    throw invalid;
+                    if (i == 5) {
+                        throw new MaximumAttemptsExceededException("Maximum attempts exceeded");
+                    } else {
+                        throw new InvalidCredentialsException("Invalid credentials");
+                    }
                 }
-                System.out.println("welcome " + username);
+                System.out.println("Welcome, " + username);
                 break;
 
-            } catch (Exception e) {
-                if (e == invalid){
-                    System.out.println("Invalid credentials");
-                }
-                if (e == maximum){
-                    System.out.println("Maximum attempts exceeded");
-                    break;
-                }
+
+            } catch (InvalidCredentialsException e) {
+
+                System.out.println(e.getMessage());
+
+
+            } catch (MaximumAttemptsExceededException e) {
+                System.out.println(e.getMessage());
+                break;
             }
         }
         scanner.close();
